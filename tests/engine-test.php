02fa142e628931e6fd8688f4fbcde46bc077a884
ArchangelDesign <archangel.raffael@gmail.com>
2015-08-05 16:16:00 +0200

    further tests

--- tests/engine-test.php
+++ tests/engine-test.php
@@ -7,7 +7,7 @@
 
 print "Testing ADB2...\n";
 
-require 'autoload_register.php';
+require '../autoload_register.php';
 
 try {
     $adb = new \ArchangelDB\ADB2();
@@ -15,3 +15,13 @@ try {
     print "Failed to create ADB \n"; die();
 }
 
+print("checking table users...\n");
+if (!$adb->tableExists('users')) {
+    print("No users table found in database.\n");
+    die();
+}
+
+print("inserting record to users...\n");
+$adb->insert('users', []);
+
+print("Test sequence completed.\n\n");
